fe0f85e23e36257af18c7a8fad4299352e373446
Thuan Vo <thuan.vo@ubc.ca>
2021-09-18 14:16:43 -0700

    Add global color styles

---
+++ lib/themes/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Basic black & white
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+
+// Blue variants
+const Color lightBlue = Color(0xFF9ECBFF);
+const Color mediumBlue = Color(0xFF64A7E4);
+const Color darkBlue = Color(0xFF52A2DC);
+
+// Green variants
+const Color lightGreen = Color(0xFFA5DFDF);
+const Color darkGreen = Color(0xFF068C8C);
+
+// Grey variants
+const Color mediumGrey = Color(0xFFE8E8E8);
+const Color lightGrey = Color(0xFFF6F6F6);
+const Color darkGrey = Color(0xFF847878);
