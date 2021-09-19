9c1725524f887f034e8a36edb15ac1310b3bcd47
nganphan123 <jill01009@gmail.com>
2021-09-19 04:53:26 -0700

    Connect to Firebase storage, integrate provider

---
+++ lib/modal.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:htn2021/databaseService.dart';
+
+class UserModal extends ChangeNotifier {
+  late String _uid;
+  late String _name;
+  String _pronoun = "";
+
+  String getUid() => _uid;
+  void updateUid(String newUid) {
+    _uid = newUid;
+    notifyListeners();
+  }
+
+  String getName() => _name;
+  void updateName(String newName) {
+    _uid = newName;
+    notifyListeners();
+  }
+
+  String getPrnoun() => _pronoun;
+  void updatePronoun(String newPro) {
+    _uid = newPro;
+    notifyListeners();
+  }
+}
